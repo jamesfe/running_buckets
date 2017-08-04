54bd65940167fe416808ed4604ad072955c8678b
jamesfe <james.ferrara@gmail.com>
2017-08-04 07:39:17 +0200

    Use  to better describe tests

--- test/test_bottomUp.js
+++ test/test_bottomUp.js
@@ -5,27 +5,36 @@ var main = require('../src/main.js');
 describe('bottomUp tests', function() {
 
   describe('calcSegment tests', function() {
-    var p1 = { distance: 100, seconds: 10 };
-    var p2 = { distance: 150, seconds: 15 };
-    var seg = main.calcSegment(p1, p2);
-    assert.equal(seg.distance, 50);
-    assert.equal(seg.time, 5);
-  });
+    it('should calculate the right value', function() {
+      var p1 = { distance: 100, seconds: 10 };
+      var p2 = { distance: 150, seconds: 15 };
+      var seg = main.calcSegment(p1, p2);
+      assert.equal(seg.distance, 50);
+      assert.equal(seg.time, 5);
+    });
 
-  describe('calcSegment tests backward', function() {
-    var p1 = { distance: 100, seconds: 10 };
-    var p2 = { distance: 150, seconds: 15 };
-    var seg = main.calcSegment(p2, p1);
-    assert.equal(seg.distance, 50);
-    assert.equal(seg.time, 5);
+    it('should calc the absolute value', function() {
+      var p1 = { distance: 100, seconds: 10 };
+      var p2 = { distance: 150, seconds: 15 };
+      var seg = main.calcSegment(p2, p1);
+      assert.equal(seg.distance, 50);
+      assert.equal(seg.time, 5);
+    });
   });
 
   describe('calcError tests', function() {
-    var s1 = { distance: 100, time: 10 };
-    var s2 = { distance: 200, time: 10 };
-    var err = main.calcError(s1, s2);
-    assert.equal(err, 300);
+    it('should calculate the right error', function() {
+      var s1 = { distance: 100, time: 10 };
+      var s2 = { distance: 200, time: 10 };
+      var err = main.calcError(s1, s2);
+      assert.equal(err, 300);
+    });
 
+    it('should calculate a zero error when values are the same', function() {
+      var s1 = { distance: 100, time: 10 };
+      var err = main.calcError(s1, s1);
+      assert.equal(err, 0);
+    });
   });
 
 
